0661a11b1e5788556a17ba1f3b9ba3875987e207
AbudiCyber <a95404829@gmail.com>
2026-07-13 14:00:52 +0300

    Repair v1.9 utilities module

--- tools-v1-9.js
+++ tools-v1-9.js
@@ -2,12 +2,14 @@
 (() => {
   "use strict";
 
-  const VERSION = "1.9.0";
-  const MULTI_PART_SUFFIXES = new Set([
-    "co.uk", "org.uk", "gov.uk", "com.au", "net.au", "co.jp", "com.br", "com.tr"
-  ]);
-
   function normalizeInput(input) {
     const value = String(input || "").trim();
     if (!value) throw new Error("EMPTY_INPUT");
-    return /^[a-z][a-z0-9+.-]*:\/\//i.test(value) ? value : `https
+    return /^[a-z][a-z0-9+.-]*:\/\//i.test(value)
+      ? value
+      : `https://${value}`;
+  }
+
+  function extractDomain(input) {
+    const url = new URL(normalizeInput(input));
+    return {
